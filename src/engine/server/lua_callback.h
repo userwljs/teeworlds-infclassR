86829a1a4fbb3afd63303b90ee15a2ff9e003e69
Alexander Akulich <akulichalexander@gmail.com>
2024-10-20 19:24:33 +0300

    Lua: Implement a template for calling LUA with a result

--- src/engine/server/lua_callback.h
+++ src/engine/server/lua_callback.h
@@ -17,11 +17,32 @@ bool RunCallback(lua_State *L, const char *pName, Args &&...args)
 	return callback.callWithHandler(LuaErrorFunc, std::forward<Args>(args)...).wasOk();
 }
 
+template<typename ResultType, class... Args>
+std::optional<ResultType> RunCallbackWithResult(lua_State *L, const char *pName, Args &&...args)
+{
+	luabridge::LuaRef callback = luabridge::getGlobal(L, pName);
+	if(!callback.isCallable())
+		return {};
+
+	luabridge::LuaResult r = callback.callWithHandler(LuaErrorFunc, std::forward<Args>(args)...);
+	if(r.size() < 2)
+		return {};
+
+	return r[1].cast<ResultType>().valueOr(ResultType());
+}
+
 #else // CONF_LUA
 
 template<class... Args>
 bool RunCallback(lua_State *L, const char *pName, Args &&...args)
 {
+	return {};
+}
+
+template<typename ResultType, class... Args>
+std::optional<ResultType> RunCallbackWithResult(lua_State *L, const char *pName, Args &&...args)
+{
+	return {};
 }
 
 #endif // CONF_LUA
