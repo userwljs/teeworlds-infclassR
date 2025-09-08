12dd1edaa22892e19eed9ae7c11ed97b42b851c7
Learath2 <learath2@gmail.com>
2025-09-08 14:02:23 +0200

    Rename timeout related functions
    (cherry picked from commit db177fa8ea7b96e0198c7316bff3242b9102b921@ddnet/ddnet)

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -4741,7 +4741,7 @@ int *CServer::GetIdMap(int ClientId)
 
 bool CServer::SetTimedOut(int ClientId, int OrigId)
 {
-	if(!m_NetServer.CanSetTimedOut(ClientId, OrigId))
+	if(!m_NetServer.HasErrored(ClientId))
 	{
 		return false;
 	}
@@ -4752,7 +4752,7 @@ bool CServer::SetTimedOut(int ClientId, int OrigId)
 		LogoutClient(OrigId, "Timeout Protection");
 	}
 
-	m_NetServer.SetTimedOut(ClientId, OrigId);
+	m_NetServer.ResumeOldConnection(ClientId, OrigId);
 
 	m_aClients[ClientId].m_Sixup = m_aClients[OrigId].m_Sixup;
 
