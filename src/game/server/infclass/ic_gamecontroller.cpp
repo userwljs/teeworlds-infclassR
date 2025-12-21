09633e37f41c99b6430fd209b8d79cb86a038419
userwljs <gaozimu_0502@163.com>
2025-12-21 15:20:35 +0800

    Fix survival respawn

--- src/game/server/infclass/ic_gamecontroller.cpp
+++ src/game/server/infclass/ic_gamecontroller.cpp
@@ -4285,6 +4285,7 @@ void CIcGameController::DoTeamChange(CPlayer *pBasePlayer, int Team, bool DoChat
 	{
 		if(Team == TEAM_SPECTATORS)
 		{
+			pPlayer->SetSurvivalRespawnTick(Server()->Tick() + Server()->TickSpeed() * Config()->m_InfSurvivalRespawnDelay);
 			GameServer()->SendChatTarget_Localization(-1, CHATCATEGORY_PLAYER, _("{str:PlayerName} joined the spectators"), "PlayerName", Server()->ClientName(ClientId), NULL);
 		}
 		else
