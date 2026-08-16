f6fb9aa85d3fa16d7d514fd58a0cc854a38ec387
userwljs <gaozimu_0502@163.com>
2026-08-16 12:44:16 +0800

    Move `CSnapIDPool` to separate compilation unit
    (follow e8305ef111b52492f225a23fa7b8c919b3c8668e@ddnet/ddnet)

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -30,6 +30,7 @@
 /* DDNET MODIFICATION END *********************************************/
 
 #include "name_ban.h"
+#include "snap_id_pool.h"
 
 #include <optional>
 #include <string>
@@ -37,47 +38,6 @@
 class CLogMessage;
 
 enum class EAccessLevel;
-
-class CSnapIdPool
-{
-	enum
-	{
-		MAX_IDS = 32 * 1024,
-	};
-
-	// State of a Snap ID
-	enum
-	{
-		ID_FREE = 0,
-		ID_ALLOCATED = 1,
-		ID_TIMED = 2,
-	};
-
-	class CID
-	{
-	public:
-		short m_Next;
-		short m_State; // 0 = free, 1 = allocated, 2 = timed
-		int m_Timeout;
-	};
-
-	CID m_aIds[MAX_IDS];
-
-	int m_FirstFree;
-	int m_FirstTimed;
-	int m_LastTimed;
-	int m_Usage;
-	int m_InUsage;
-
-public:
-	CSnapIdPool();
-
-	void Reset();
-	void RemoveFirstTimeout();
-	int NewId();
-	void TimeoutIds();
-	void FreeId(int Id);
-};
 
 class CServerBan : public CNetBan
 {
